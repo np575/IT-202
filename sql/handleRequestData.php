1ede32d820fa35778228b4de71de1ec727c2101c
np575 <np575@njit.edu>
2019-09-28 20:30:24 -0400

    updating file

--- sql/handleRequestData.php
+++ sql/handleRequestData.php
@@ -7,12 +7,37 @@ if(isset($_GET['name'])){
 
 if(isset($_GET['number'])){
 	$number = $_GET['number'];
-	echo "<br>" . $number . " should be a number..";
-	echo "<br>but it might not be<br>";
+        if(is_numeric($number)){
+	echo "<br>" . $number . " is a number..<br> ";
+        }
+        else{
+	echo "<br>"  . $number . "  is not  a number<br>";
+	}
 }
 if(isset($_GET['number1'])){
-	$number1 = $_GET['number1'];
-	echo "<br>" . $number1 . " should be a number...";
-	echo "<br>but it might not be<br>";
+        $number1 = $_GET['number1'];
+       	if(is_numeric($number1)){
+        echo "<br>" . $number1 . " is a number..<br> ";
+       	}  
+       	else{
+        echo "<br>"  . $number1 . " is  not a number <br>";
+        }
 }
+
+$a=is_numeric($number);
+$b=is_numeric($number1);
+
+if (($a== TRUE) and ($b == TRUE)){
+echo "<br>"  . "You can do sum ";
+$sum = $number + $number1;
+echo "<br> sum: " . $sum;
+
+}
+else {
+echo "<br>" . " You can't do sum <br>";
+
+}
+
+
+echo "<br> concatenating strings: " .$number .$number1;
 ?>
