2d252d41f73e1e493ba7f3d27a1ca25bcdc2ff3e
np575 <np575@njit.edu>
2019-10-03 11:10:36 -0400

    form php

---
+++ form01.php
@@ -0,0 +1,24 @@
+<?php
+function getName(){
+	if(isset($_GET['name'])){
+		echo "<p>Hello, " . $_GET['name'] . "</p>";
+	}
+}
+?>
+<html>
+<head></head>
+<body><?php getName();?>
+<form mode="GET" action="#">
+<input name="name" type="text" placeholder="Enter your name"/>
+
+
+
+<input type="submit" value="try it"/>
+</form>
+</body>
+</html>
+
+<?php
+if(isset($_get)){
+		echo "<br><pre>" . var_export($_GET, true). "</pre><br>";
+}
